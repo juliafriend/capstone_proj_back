b40ac12873169c96505c393935f31e40db7e1360
Julia Friend <juliafriend@Julias-MacBook-Pro.local>
2022-09-23 09:37:56 -0400

    updated application

--- demo 2/src/main/java/com/example/demo/PackApplication.java
+++ demo 2/src/main/java/com/example/demo/PackApplication.java
@@ -2,11 +2,7 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-// import org.springframework.context.annotation.Bean;
-// import org.springframework.web.servlet.config.annotation.CorsRegistry;
-// import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -27,23 +23,12 @@ public class PackApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(PackApplication.class, args);
 	}
-	// @Bean
-	// public WebMvcConfigurer corsConfigurer() {
-	// 	return new WebMvcConfigurer() {
-	// 		@Override
-	// 		public void addCorsMappings(CorsRegistry registry) {
-	// 			registry.addMapping("/pack").allowedOrigins("http://localhost:8080");
-	// 		}
-	// 	};
-	// }
 
-	@CrossOrigin(origins = "http://localhost:8080")
-	@GetMapping("/pack")
+	@GetMapping("/")
 	public Iterable<PackList> index() {
 		return packlistRepository.findAll();
 	}
 	
-
 	@PostMapping("/pack")
 	public Iterable<PackList> create (@RequestBody PackList packlistData) {
 		packlistRepository.save(packlistData);
